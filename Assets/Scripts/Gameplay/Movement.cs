8ef2da6d718d4a918cfb19f0f05167ab4f05bd78
Jesus Guio <jfranciscoguio@protonmail.com>
2021-08-06 09:43:40 -0500

    Add Obstacles to Race map

--- Assets/Scripts/Gameplay/Movement.cs
+++ Assets/Scripts/Gameplay/Movement.cs
@@ -7,6 +7,8 @@ namespace Gameplay
     {
         private Transform _transform;
         private float _yPosition;
+        private bool _penaltyActivated = false;
+        private float _penaltyTimeRemaining;
 
         [SerializeField] public float speed;
         [SerializeField] public float angularVelocity;
@@ -21,6 +23,19 @@ namespace Gameplay
         // Update is called once per frame
         void Update()
         {
+            if (_penaltyActivated)
+            {
+                if (_penaltyTimeRemaining <= 0)
+                {
+                    _penaltyActivated = false;
+                }
+                else
+                {
+                    _penaltyTimeRemaining -= Time.deltaTime;
+                    return;
+                }
+            }
+            
             Vector3 rotation = Vector3.zero;
             Vector3 position = _transform.position;
             rotation.y = _transform.eulerAngles.y;
@@ -48,6 +63,20 @@ namespace Gameplay
             _transform.eulerAngles = rotation;
             position.y = _yPosition;
             _transform.position = position;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (_penaltyActivated) return;
+
+            Obstacle obstacle = other.gameObject.GetComponent<Obstacle>();
+
+            if (obstacle)
+            {
+                _penaltyActivated = true;
+                _penaltyTimeRemaining = obstacle.penaltyTime;
+                Object.Destroy(other.gameObject);
+            }
         }
     }
 }
---
+++ Assets/Scripts/Gameplay/Obstacle.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Gameplay
+{
+    public class Obstacle : MonoBehaviour
+    {
+        [SerializeField] public int penaltyTime;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
